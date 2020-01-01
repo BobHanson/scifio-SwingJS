f5127bbcd8e380a8a33c66e90313512ebfc89585
BobHanson <hansonr@stolaf.edu>
2020-01-01 12:02:51 -0600

    scifio issue #415 early check for reading file that does not exist.

--- src/main/java/io/scif/img/ImgOpener.java
+++ src/main/java/io/scif/img/ImgOpener.java
@@ -65,6 +65,7 @@ import net.imglib2.type.Type;
 
 import org.scijava.Context;
 import org.scijava.app.StatusService;
+import org.scijava.io.location.FileLocation;
 import org.scijava.io.location.Location;
 import org.scijava.plugin.Parameter;
 
@@ -379,6 +380,9 @@ public class ImgOpener extends AbstractImgIOComponent {
 
 		ReaderFilter r = null;
 		try {
+			if (source instanceof FileLocation && !((FileLocation)source).getFile().exists()) {
+				throw new IOException("File does not exist: " + source);
+			}
 			r = initializeService.initializeReader(source, config);
 			r.enable(ChannelFiller.class);
 			r.enable(PlaneSeparator.class).separate(axesToSplit(r));
